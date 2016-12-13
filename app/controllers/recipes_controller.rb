e74714b724ba7d2e846f0b8a37a8b6df6cd3bd09
Rafeeq Wilson <rafeeqwilson914@gmail.com>
2016-12-13 13:32:12 -0800

    added basic layout to controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,44 @@ class RecipesController < ApplicationController
 	def index
 		@recipes = Recipe.all
 	end
+
+	def new
+		@recipe = Recipe.new
+	end
+
+	def create
+		recipe = Recipe.new(recipe_params)
+		if recipe.save
+			redirect_to recipe
+		else
+			render "new"
+		end	
+	end
+
+	def show
+		find_params_id
+	end
+
+	def edit
+		find_params_id
+	end
+
+	def update
+		find_params_id
+		if recipe.update(recipe_params)
+		 	redirect_to @recipe
+		else
+		 	render "show"
+		end	
+	end
+
+	private
+
+	def recipe_params
+		params.require(:recipe).permit(:name_of_ingredient, :quantity)
+	end
+
+	def find_params_id
+		@recipe = Recipe.find(params[:id])
+	end	
 end
